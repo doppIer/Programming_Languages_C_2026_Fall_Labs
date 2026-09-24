86fb55e647a01bc4bedd2eec45163269c92b18b7
Bedirhan <72643911+doppIer@users.noreply.github.com>
2026-09-24 09:14:38 +0000

    Complete Lab 2 tasks

--- src/hello.c
+++ src/hello.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   // This is your first C program my friend
   printf("Hello from Bedirhan\n");
   printf("You passed %d argument(s).\n", argc - 1);
